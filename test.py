53d1421c5de086752725c18f6272a399c64e406c
Moss <danielsmoss96@gmail.com>
2022-08-15 20:57:35 +0100

    Implement HandleMatch class, update testing

--- test.py
+++ test.py
@@ -2,67 +2,49 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
+input("This is the standard GET user")
 response = requests.get(BASE + 'user/Bean')
 print(response.json())
-input()
 
+input("This is a PUT for a user that already exists")
 response = requests.put(BASE + 'user/Moss', {
     "password": "Hello",
     "email": "anotherfakeemail@gmail.net"
 })
 print(response.json())
-input()
 
-response = requests.patch(BASE + 'user/Moss/details', {
-    "bio": "Pretty much just the coolest guy ever"
-})
-print(response.json())
-
-response = requests.patch(BASE + 'user/Moss/details', {
-    "gender": "male"
-})
-print(response.json())
-
-response = requests.patch(BASE + 'user/Moss/details', {
-    "location": "Liverpool"
-})
-print(response.json())
-
-response = requests.patch(BASE + 'user/Moss/details', {
-    "interests": '["gaming", "coding", "rugby", "reading", "music"]'
-})
-print(response.json())
-input()
-
+input("This is a PUT for a user with insufficient data")
 response = requests.put(BASE + 'user/Morp', {
     "password": "Hello"
 })
 print(response.json())
-input()
 
+input("Bad details updates")
 response = requests.patch(BASE + 'user/Bean/details')
 print(response.json())
 response = requests.patch(BASE + 'user/Bean/details', {
     "cheese": "Yes please, Gromit"
 })
 print(response.json())
+
+input("users with gaming query")
+response = requests.get(BASE + '/users?interest=gaming')
+print(response.json())
+
+input("Users with coding query")
+response = requests.get(BASE + '/users?interest=coding')
+print(response.json())
+
+input("Users with no query")
+response = requests.get(BASE + '/users')
+print(response.json())
 input()
 
-response = requests.patch(BASE + 'user/Bean/incoming_likes', {
-    "incoming_like": "Moss",
+username = input("The user you want to like: ")
+incoming_like = input("Your username: ")
+response = requests.patch(BASE + 'user/' + username + '/incoming_likes', {
+    "incoming_like": incoming_like,
     "liked_detail": "bio",
     "opening_message": "I like beans"
 })
-print(response.json())
-input()
-
-response = requests.get(BASE + '/users?interest=gaming')
-print(response.json())
-input()
-
-response = requests.get(BASE + '/users?interest=coding')
-print(response.json())
-input()
-
-response = requests.get(BASE + '/users')
 print(response.json())
